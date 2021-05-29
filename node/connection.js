474f26ca6f7041160d6fea638c67d53b7acc4934
Kaushal <kaushal.shah@actonate.com>
2021-05-29 16:00:45 +0530

    pass updated

--- node/connection.js
+++ node/connection.js
@@ -1,23 +1,23 @@
-let mysql = require("mysql");
-let express = require("express");
-let bodyParser = require("body-parser");
-let cors = require("cors");
+let mysql = require('mysql');
+let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors');
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 let connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "0168",
-  database: "kt",
+  user: 'root',
+  password: 'your password',
+  database: 'kt',
 });
 
 connection.connect((err) => {
   if (!err) {
-    console.log("Database is connected..");
-  } else console.log("Error Connecting a Database...", err);
+    console.log('Database is connected..');
+  } else console.log('Error Connecting a Database...', err);
 });
 exports.connection = connection;
 exports.app = app;
